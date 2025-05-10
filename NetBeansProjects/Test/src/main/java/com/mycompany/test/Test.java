747b0eb804989f57af79e296eb82131c5df7b6cd
deniyads05 <deniyads@gmail.com>
2025-05-10 11:21:37 +0530

    Add files via upload

---
+++ NetBeansProjects/Test/src/main/java/com/mycompany/test/Test.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.test;
+
+/**
+ *
+ * @author IT20
+ */
+public class Test {
+
+     static int factorial(int n)
+    {
+        if (n == 0)
+            return 1;
+
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args)
+    {
+        int num = 5;
+        System.out.println("Factorial of " + num + " is "
+                           + factorial(5));
+    }
+       
+        }
